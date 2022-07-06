992589923d05bd922da646bf7cae82e8e814a23c
DwifteJB <dwifte@outlook.com>
2022-07-06 23:19:33 +0100

    1.5.4

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # CydiaRepoDownloader
 **Simple Cydia Repo installer and parser for NodeJS**
 
+**Like this? Check out the <a src="https://github.com/DwifteJB/CyDesktop">CyDesktop</a> application being made with this package!**
 ## Usage
 
 ### Parse Repository as JSON
 
     import * as CRD from 'cydiarepodownloader';
     const repoData = await CRD.parseRepo(repoURL);
-    // This creates a JSON stingified format. An example is provided in: "example.json" (using the cercube repo)
+    // This creates a JSON stingified format. An example is provided in: "example.json"
     /* OUTPUTS:
         onError: false
         onSuccess: true
     /*
+
+### Parse Repository as JSON (Packages ONLY)
+
+    import * as CRD from 'cydiarepodownloader';
+    const repoData = await CRD.parsePackages(repoURL);
+    // This creates a JSON stingified format. An example is provided in: "example.json" (only the packages are saved in this mode.)
+    /* OUTPUTS:
+        onError: false
+        onSuccess: true
+    /*
+
 
 ### Download Package from Repo
 
@@ -28,4 +40,6 @@
     await CRD.downloadAllPackagesFromRepo(repoURL, directoryToDownload);
     /* OUTPUTS:
         Errors: ["packagename1","packagename2"]
+
+        (if errors are empty, you get an empty array)
     /*
--- index.js
+++ index.js
@@ -9,13 +9,21 @@ const streamPipeline = util.promisify(stream.pipeline);
 //     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 // }
 const headers = {"X-Machine": "iPhone13,4","X-Firmware": "15.0","Proxy-Connection": "keep-alive","Cache-Control": "max-age=0","User-Agent": "Telesphoreo APT-HTTP/1.0.592","X-Unique-ID": "iLikeBlueBabyyy","Connection": "keep-alive"}
+
 export async function getRepoImage(repoURL) {
     return await fetch(repoURL+"/CydiaIcon.png", {
         headers: headers
     }); 
 }
+export function isRepo(repoURL) {
+    fetch(repoURL + "/CydiaIcon.png").then(function(){
+        return true;
+    }).catch(function(){
+        return false;
+    })
+}
 export async function downloadPackageFromRepo(repoURL, packageName, downloadLocation) {
-    const repo = await this.parseRepo(repoURL);
+    const repo = await this.parsePackages(repoURL);
     const repoDownload = JSON.parse(repo);
     for (var pkg in repoDownload.packages) {
         if (repoDownload.packages[pkg].Name.toLowerCase() == packageName.toLowerCase()) {
@@ -36,7 +44,7 @@ export async function downloadPackageFromRepo(repoURL, packageName, downloadLoca
 }
 export async function downloadAllPackagesFromRepo(repoURL, downloadLocation) {
     console.log("Downloading all packages to: " + downloadLocation);
-    const repoDownload = JSON.parse(await this.parseRepo(repoURL));
+    const repoDownload = JSON.parse(await this.parsePackages(repoURL));
     let errors = []
     for (var pkg in repoDownload.packages) {
         try {
@@ -110,7 +118,84 @@ export async function parseRepo(repoURL) {
     pkgs = pkgs.replace('Filename:pool', `Filename: ${repoURL}/pool`)
     pkgs = pkgs.replace('Filename:files/', `Filename: ${repoURL}/files`)
     pkgs = pkgs.replace('�', ' ');
-    let data = {"url": repoURL,"packages": []};
+
+    // Release data
+    const release = await fetch(repoURL+"/Release", {
+        headers: headers
+    }).catch(function(){
+        return false;
+    });
+    let parsedRelease = await debianCtrl.parse(await release.text());
+    let data = {
+        "name": parsedRelease.Label,
+        "version": parsedRelease.Version,
+        "codename": parsedRelease.codename,
+        "architecture": parsedRelease.Architectures,
+        "iconURL": repoURL + "/CydiaIcon.png",
+        "url": repoURL,
+        "packages": []
+};
+    // Parse that control file.
+    const packages = pkgs.split(/(\n\n)/g).filter((p) => p.trim());
+    for (var pkgzs in packages) {
+        let parsedData = debianCtrl.parse(packages[pkgzs]);
+        data.packages.push(parsedData);
+    }
+    fs.rmSync(tempDir,{ recursive: true, force: true});
+    return JSON.stringify(data,null,2)
+}
+export async function parsePackages(repoURL) {
+    let skip = false;
+    const tempDir = fs.mkdtempSync("repoDownloader")
+    try {
+        const response = await fetch(repoURL+"/Packages", {
+            headers: headers
+        })
+        await streamPipeline(response.body,fs.createWriteStream(tempDir + "/repo.pkgs"));
+        skip = true
+    } catch (e) {
+        // Not procursus, so skip.
+        skip = false
+    }
+    if (skip == false) {
+        try {
+            const response = await fetch(repoURL+"/Packages.bz2", {
+                headers: headers
+            })
+            await streamPipeline(response.body,fs.createWriteStream(tempDir + "/Packages.bz2"));
+
+        } catch (e) {
+            console.log(`[ ERROR ]: Repo had an error downloading the BZ2.`);
+            return false;
+        }
+        // Unzip BZ2
+        try {
+            var compressedData = fs.readFileSync(tempDir + "/Packages.bz2")
+            var decompressed = Bunzip.default.decode(compressedData);
+            fs.writeFileSync(tempDir + "/repo.pkgs", decompressed);
+        } catch (e) {
+            console.log("[ ERROR ]: Couldn't unzip BZ2.");
+            return false
+        }
+    } 
+    // Now that the BZ2 has been decompressed to pkgs, we need to read those pkgs
+    let pkgs = fs.readFileSync(tempDir + "/repo.pkgs").toString();
+    pkgs = pkgs.replace('Filename: ./debs', `Filename: ${repoURL}/debs`)
+    pkgs = pkgs.replace('Filename: ./deb', `Filename: ${repoURL}/deb`)
+    pkgs = pkgs.replace('Filename: deb', `Filename: ${repoURL}/deb`)
+    pkgs = pkgs.replace('Filename: debs', `Filename: ${repoURL}/debs`)
+    pkgs = pkgs.replace('Filename: api', `Filename: ${repoURL}/api`)
+    pkgs = pkgs.replace('Filename: pool', `Filename: ${repoURL}/pool`)
+    pkgs = pkgs.replace('Filename: files', `Filename: ${repoURL}/files`)
+    pkgs = pkgs.replace('Filename:./debs', `Filename: ${repoURL}/debs`)
+    pkgs = pkgs.replace('Filename:./deb', `Filename: ${repoURL}/deb`)
+    pkgs = pkgs.replace('Filename:deb', `Filename: ${repoURL}/deb`)
+    pkgs = pkgs.replace('Filename:debs', `Filename: ${repoURL}/debs`)
+    pkgs = pkgs.replace('Filename:api', `Filename: ${repoURL}/api`)
+    pkgs = pkgs.replace('Filename:pool', `Filename: ${repoURL}/pool`)
+    pkgs = pkgs.replace('Filename:files/', `Filename: ${repoURL}/files`)
+    pkgs = pkgs.replace('�', ' ');
+    let data = {"packages": []};
     // Parse that control file.
     const packages = pkgs.split(/(\n\n)/g).filter((p) => p.trim());
     for (var pkgzs in packages) {
